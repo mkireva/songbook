e26a9d9a92878b8a016854f76ec09d9faa73dbb1
mkireva <mariya@kireva.de>
2024-06-10 19:30:30 +0200

    fix: footer wip

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -5,11 +5,11 @@ export default function Footer() {
   return (
     <footer className="w-full  bg-gray-200 py-6 dark:bg-gray-800">
       <div className="container mx-auto flex items-center justify-between px-4 md:px-6">
-        <div className="flex items-center space-x-4">
+        <div className="flex items-center space-x-4  ">
         
           <Link
             href="https://www.kireva.de/"
-            className="text-lg text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+            className="text-lg text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 ml-6"
             prefetch={false}
             target="_blank"
           >
