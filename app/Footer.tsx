9f5c936105f7f47cb7b7840e299277741a1d556a
mkireva <mariya@kireva.de>
2024-06-13 20:11:26 +0200

    fix: footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -5,17 +5,15 @@ export default function Footer() {
   return (
     <>
       <footer className="w-full py-6 text-gray-800 dark:bg-gray-800">
-        <div className="container mx-auto flex items-center justify-between px-4 md:px-6">
-          <div className="flex items-center space-x-4  "></div>
+        <div className="container mx-auto items-center justify-between px-4 md:px-6">
           <Link
             href="https://www.kireva.de/"
-            className="text-lg text-gray-800 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 ml-6"
+            className="text-lg text-gray-800 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
             prefetch={false}
             target="_blank"
           >
             &copy; {year} Песни от Учителя
           </Link>
-          <div className="flex items-center space-x-4"></div>
         </div>
       </footer>
     </>
