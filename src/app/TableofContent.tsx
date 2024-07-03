e1c210862ffb40ba52b17e34c6afd9b14b63c2b3
mkireva <mariya@kireva.de>
2024-07-03 19:28:39 +0200

    add: trans

--- src/app/TableofContent.tsx
+++ src/app/TableofContent.tsx
@@ -6,42 +6,44 @@ import {
   TableHeader,
   TableRow,
 } from "../components/ui/table";
+import { useTranslations } from "next-intl";
 
 import data from "data/data.json";
 import AudioPlayer from "./AudioPlayer";
 import { ScrollArea } from "components/scroll-area";
-
 export default function TableofContent() {
+  const t = useTranslations(); // Corrected
   return (
     <div className="md:w-1/2 p-4 mx-auto sm:p-0 my-10">
       <h1 className="text-center text-2xl font-medium leading-tight text-muted-foreground dark:text-white tracking-tighter md:text-2xl lg:leading-[1.1]">
-        Съдържание
+        {t("table.heading")}
       </h1>
       <ScrollArea className="h-[600px] rounded-md border mt-10">
-
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Play</TableHead>
-            <TableHead>Заглавие</TableHead>
-            <TableHead className="text-right">Стр.</TableHead>
-            {/* <TableHead className="text-right">Int</TableHead> */}
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {data.map((song) => (
-              <TableRow key={song.id}>
-              <TableCell>
-                {song.play ? <AudioPlayer audio={song.audio ?? ""} /> : null}
-              </TableCell>
-              <TableCell>{song.title}</TableCell>
-              <TableCell className="text-right text-sm">{song.page}</TableCell>
-              {/* <TableCell className="text-right">{song.interpret}</TableCell> */}
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>{t("table.play")}</TableHead>
+              <TableHead>{t("table.title")}</TableHead>
+              <TableHead className="text-right">{t("table.page")}</TableHead>
+              {/* <TableHead className="text-right">Int</TableHead> */}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-          </ScrollArea>
+          </TableHeader>
+          <TableBody>
+            {data.map((song) => (
+              <TableRow key={song.id}>
+                <TableCell>
+                  {song.play ? <AudioPlayer audio={song.audio ?? ""} /> : null}
+                </TableCell>
+                <TableCell>{song.title}</TableCell>
+                <TableCell className="text-right text-sm">
+                  {song.page}
+                </TableCell>
+                {/* <TableCell className="text-right">{song.interpret}</TableCell> */}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </ScrollArea>
     </div>
   );
 }
