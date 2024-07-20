89f798648dd82136d604ab1980bd686ca116e452
mkireva <mariya@kireva.de>
2024-07-20 11:35:08 +0200

    fix: song player

--- src/app/[locale]/songs/page.tsx
+++ src/app/[locale]/songs/page.tsx
@@ -21,7 +21,7 @@ export default function Songs() {
         {t("title")}
       </h2>
       <div className="w-full xl:flex items-center gap-x-4 justify-center">
-        <div className="relative mt-6 mb-2 mr-2 ml-2 md:w-5/12 h-[300px] items-center justify-center">
+        {/* <div className="relative mt-6 mb-2 mr-2 ml-2 md:w-5/12 h-[300px] items-center justify-center">
           <Image
             src="/zigulka.jpg"
             fill
@@ -29,21 +29,17 @@ export default function Songs() {
             alt=""
             className="rounded-lg border-none"
           />
-        </div>
-      </div>
-      <div className="block text-muted-foreground text-xs md:text-sm font-semibold antialiased leading-normal text-center">
-        Музика от Учителя - Интеграл <br />
-        Интерпрет: Петър Ганев
+        </div> */}
       </div>
 
-      <div className="w-full rounded-[10px] flex flex-col xl-flex-row items-center p-3 xl:pl-20 xl:pr-20 gap-x-12">
+      <div className="w-full rounded-[10px] flex flex-col xl-flex-row items-center p-8 xl:pl-20 xl:pr-20 gap-x-12">
         {/* track container */}
         <div className="flex flex-1 w-full">
           {/* tracks */}
-          <div className="flex-1 flex flex-col xl:px-12">
+          <div className="flex-1 flex flex-col xl:px-12 justify-center items-center">
             {data.map((song, index) => {
               return (
-                <div className="flex flex-1 w-full" key={index}>
+                <div className="flex flex-1 w-full md:w-1/2 " key={index}>
                   {/* track name */}
                   <div className="flex flex-1 items-center gap-x-2 font-semibold xl:font-extrabold pr-2">
                     {song.play ? (
@@ -52,16 +48,17 @@ export default function Songs() {
                   </div>
 
                   {/* track player */}
-                  <div className="md:w-3/4">
+                  <div className="">
                     {song.play ? (
                       <AudioPlayer
                         volume={40}
                         volumePlacement="top"
                         preload="none"
-                        className="custom-style album-player"
+                        className="custom-style  album-player"
                         src={song.audio}
                         onPlay={handlePlay}
                         onPause={handlePause}
+                        
                         style={{
                           backgroundColor: "transparent",
                           boxShadow: "none",
@@ -74,6 +71,10 @@ export default function Songs() {
               );
             })}
           </div>
+        </div>
+        <div className="block text-muted-foreground mt-4 text-xs md:text-sm font-semibold antialiased leading-normal text-center">
+          Музика от Учителя - Интеграл <br />
+          Интерпрет: Петър Ганев
         </div>
       </div>
     </div>
