6d2bcb02a60684f0dbccc27c156675d2bbce5488
mkireva <mariya@kireva.de>
2024-10-04 14:10:25 +0200

    fix: about page

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -20,43 +20,40 @@ export async function generateMetadata({
 export default function About() {
   const t = useTranslations("About");
   return (
-    <>
-      <div className="flex flex-col min-h-screen items-center ">
-        <div className="container lg:w-3/4 p-4">
-          <div className="mt-10 mb-10 text-center ">
-          <h2 className="text-center text-primary dark:text-white text-3xl font-medium leading-tight tracking-tighter md:text-3xl lg:leading-[1.1] mt-6">{t("title")}</h2>
-          </div>
-          <div className="p-2">
-            <p className="text-md">{t("content.paragraph1")}</p>
-            <p className="text-md mt-5">{t("content.paragraph2")}</p>
-
-            <p className="text-md mt-5">{t("content.paragraph3")}</p>
-
-            <p className="text-md mt-5">{t("content.paragraph4")}</p>
-            <p className="text-md mt-5">{t("content.paragraph5")}</p>
-
-            <ul className="list-decimal list-inside mt-5">
-              <li className="text-md mt-5 ">{t("content.list1")}</li>
-              <li className="text-md mt-5">{t("content.list2")}</li>
-              <li className="text-md mt-5">{t("content.list3")}</li>
-            </ul>
-            <p className="text-md mt-5">{t("content.paragraph6")}</p>
-            <p className="text-md mt-5">{t("content.paragraph7")}</p>
-            <ul className="list-decimal list-inside mt-5">
-              <li className="text-md mt-5">{t("content.list4")}</li>
-              <li className="text-md mt-5">{t("content.list5")}</li>
-              <li className="text-md mt-5">{t("content.list6")}</li>
-            </ul>
-            <p className="text-md mt-5">{t("content.paragraph8")}</p>
-            <p className="text-md mt-5">{t("content.paragraph9")}</p>
-            <p className="text-md mt-5">{t("content.paragraph10")}</p>
-            <p className="text-md mt-5 text-end mr-3">
-              {t("content.paragraph11")}
-            </p>
-            <p className="text-md text-end">{t("content.paragraph12")}</p>
-          </div>
+    <div className="flex flex-col min-h-screen items-center ">
+      <div className="container lg:w-4/5 p-4">
+        <div className="mt-10 mb-10 text-center ">
+          <h2 className="text-center text-primary dark:text-white text-3xl font-medium leading-tight tracking-tighter md:text-3xl lg:leading-[1.1] mt-6">
+            {t("title")}
+          </h2>
+        </div>
+        <div className="p-2">
+          <p className="text-md">{t("content.paragraph1")}</p>
+          <p className="text-md mt-5 text-balance">{t("content.paragraph2")}</p>
+          <p className="text-md mt-5 text-balance">{t("content.paragraph3")}</p>
+          <p className="text-md mt-5 text-balance">{t("content.paragraph4")}</p>
+          <p className="text-md mt-5 text-balance">{t("content.paragraph5")}</p>
+          <ul className="list-decimal list-inside mt-5">
+            <li className="text-md mt-5 ">{t("content.list1")}</li>
+            <li className="text-md mt-5">{t("content.list2")}</li>
+            <li className="text-md mt-5">{t("content.list3")}</li>
+          </ul>
+          <p className="text-md mt-5 text-balance">{t("content.paragraph6")}</p>
+          <p className="text-md mt-5 text-balance">{t("content.paragraph7")}</p>
+          <ul className="list-decimal list-inside mt-5">
+            <li className="text-md mt-5">{t("content.list4")}</li>
+            <li className="text-md mt-5">{t("content.list5")}</li>
+            <li className="text-md mt-5">{t("content.list6")}</li>
+          </ul>
+          <p className="text-md mt-5 text-balance">{t("content.paragraph8")}</p>
+          <p className="text-md mt-5 text-balance">{t("content.paragraph9")}</p>
+          <p className="text-md mt-5">{t("content.paragraph10")}</p>
+          <p className="text-md mt-5 text-end mr-3">
+            {t("content.paragraph11")}
+          </p>
+          <p className="text-md text-end">{t("content.paragraph12")}</p>
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,71 +6,68 @@ import { Button } from "components/ui/button";
 export default function Home() {
   const t = useTranslations("Home");
   return (
-    <>
-      <div className="min-h-screen">
-        <div className="container">
-          <h2 className="text-center text-2xl font-medium leading-tight text-primary dark:text-white  tracking-tighter md:text-2xl lg:leading-[1.1] mt-10">
-            {t("author")}
-          </h2>
-          <h1 className="text-center text-4xl font-medium leading-tight text-primary dark:text-white tracking-tighter md:text-4xl lg:leading-[1.1] mt-6">
-            {t("title")}
-          </h1>
-          <h1 className="text-md p-3 text-muted-foreground text-center mt-10">
+    <div className="min-h-screen">
+      <div className="container">
+        <h2 className="text-center text-2xl font-medium leading-tight text-primary dark:text-white  tracking-tighter md:text-2xl lg:leading-[1.1] mt-10">
+          {t("author")}
+        </h2>
+        <h1 className="text-center text-4xl font-medium leading-tight text-primary dark:text-white tracking-tighter md:text-4xl lg:leading-[1.1] mt-6">
+          {t("title")}
+        </h1>
+        <h1 className="text-md p-3 text-muted-foreground text-center mt-10">
           {t("publisher")} <br />
-            <span className="space-x-2">
-              <span>{t("publisher1")}</span>
-              <span className="font-bold text-2xl">·</span>
-              <span>{t("publisher2")}</span>
-            </span>
-          </h1> 
-          <h1 className="text-lg italic text-center text-muted-foreground mt-2">
-            {t("edition")}
-          </h1>
-          <h1 className="text-lg text-center text-muted-foreground mt-3">
-            {t("year")}
-          </h1>
-          <div className="max-w-5xl grid md:grid-cols-3 sm:grid-cols-1 gap-2 mx-auto my-12 ">
-            <div className="my-6 ">
-              <Image
-                src="/cover.jpeg"
-                width={500}
-                height={500}
-                quality={100}
-                style={{ width: "100vw", height: "auto" }} // Maintain aspect ratio
-                priority={true}
-                alt="Picture of the author"
-                className="shadow-xl rounded-xl"
-              />
-            </div>
-            <div className="md:col-span-2 my-4 max-w-4xl mx-auto p-2">
-              <p className=" md:ml-6 text-md text-start">{t("description")}</p>
-              <p className=" mt-5 md:ml-6 text-md text-start">
-                {t("interactive_pdf")}
-              </p>
-              <ul className="mt-3 pl-5 pr-5 list-disc">
-                <li className="mt-1 md:ml-6 text-md">{t("feature1")}</li>
-                <li className="md:ml-6 text-md">{t("feature2")}</li>
-                <li className="md:ml-6 text-md">{t("feature3")}</li>
-                <li className="md:ml-6 text-md">{t("feature4")}</li>
-                <li className="md:ml-6 text-md">{t("feature5")}</li>
-              </ul>
-              <div className="my-8 flex items-center justify-center ">
-                <Button className="md:w-3/5 w-full py-5 md:mt-10 lg:mt-10 xl:mt-10">
-                  <a
-                    className="text-lg"
-                    href={"/Песни-от-Учителя-2024.pdf"}
-                    target="_blank"
-                  >
-                    {t("download")}
-                  </a>
-                </Button>
-              </div>
+          <span className="space-x-2">
+            <span>{t("publisher1")}</span>
+            <span className="font-bold text-2xl">·</span>
+            <span>{t("publisher2")}</span>
+          </span>
+        </h1>
+        <h1 className="text-lg italic text-center text-muted-foreground mt-2">
+          {t("edition")}
+        </h1>
+        <h1 className="text-lg text-center text-muted-foreground mt-3">
+          {t("year")}
+        </h1>
+        <div className="max-w-5xl grid md:grid-cols-3 sm:grid-cols-1 gap-2 mx-auto my-12 ">
+          <div className="my-6 ">
+            <Image
+              src="/cover.jpeg"
+              width={500}
+              height={500}
+              quality={100}
+              style={{ width: "100vw", height: "auto" }} // Maintain aspect ratio
+              priority={true}
+              alt="Picture of the author"
+              className="shadow-xl rounded-xl"
+            />
+          </div>
+          <div className="md:col-span-2 my-4 max-w-4xl mx-auto p-2">
+            <p className=" md:ml-6 text-md text-start">{t("description")}</p>
+            <p className=" mt-5 md:ml-6 text-md text-start">
+              {t("interactive_pdf")}
+            </p>
+            <ul className="mt-3 pl-5 pr-5 list-disc">
+              <li className="mt-1 md:ml-6 text-md">{t("feature1")}</li>
+              <li className="md:ml-6 text-md">{t("feature2")}</li>
+              <li className="md:ml-6 text-md">{t("feature3")}</li>
+              <li className="md:ml-6 text-md">{t("feature4")}</li>
+              <li className="md:ml-6 text-md">{t("feature5")}</li>
+            </ul>
+            <div className="my-8 flex items-center justify-center ">
+              <Button className="md:w-3/5 w-full py-5 md:mt-10 lg:mt-10 xl:mt-10">
+                <a
+                  className="text-lg"
+                  href={"/Песни-от-Учителя-2024.pdf"}
+                  target="_blank"
+                >
+                  {t("download")}
+                </a>
+              </Button>
             </div>
           </div>
         </div>
-        <TableofContent />
-        
       </div>
-    </>
+      <TableofContent />
+    </div>
   );
 }
